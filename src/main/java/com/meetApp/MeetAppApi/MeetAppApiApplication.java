b6e48833b8217ad7aacec84cdd3b306952737e45
unknown <rubenrodriguezarnedillo@gmail.com>
2022-06-20 18:19:40 +0200

    primer commit

---
+++ src/main/java/com/meetApp/MeetAppApi/MeetAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.meetApp.MeetAppApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetApp/MeetAppApi/MeetAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetApp.MeetAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
