b1c1ff606c55f7b225a44834894bbfd22f022917
Mery05 <freiria_may@hotmail.com>
2022-06-21 19:17:34 +0200

    Métodos para recordatorios

--- src/main/java/com/meetApp/MeetAppApi/controller/RemindController.java
+++ src/main/java/com/meetApp/MeetAppApi/controller/RemindController.java
@@ -1,5 +1,7 @@
 package com.meetApp.MeetAppApi.controller;
 
+import com.meetApp.MeetAppApi.domain.Reminder;
+import com.meetApp.MeetAppApi.domain.User;
 import com.meetApp.MeetAppApi.service.MessageService;
 import com.meetApp.MeetAppApi.service.RemindService;
 import com.meetApp.MeetAppApi.service.UserService;
@@ -7,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RemindController {
@@ -19,5 +23,37 @@ public class RemindController {
 
     @Autowired
     private UserService userService;
+
+
+    // Lista todos los recordatorios
+    @GetMapping("/reminders")
+    public List<Reminder> getReminders() {
+        List<Reminder> reminders;
+
+        reminders = remindService.findAllReminders();
+
+        return reminders;
+    }
+
+    // Borra un recordatorio
+    @DeleteMapping("/reminders/{id}")
+    public Reminder deleteReminder(@PathVariable long id) {
+        Reminder reminder = remindService.deleteReminder(id);
+        return reminder;
+    }
+
+    // Registra un nuevo recordatorio
+    @PostMapping("/reminder")
+    public Reminder addReminder(@RequestBody Reminder reminder) {
+        Reminder newReminder = remindService.addReminder(reminder);
+        return newReminder;
+    }
+
+    // Modifica un recordatorio
+    @PutMapping("/reminder/{id}")
+    public Reminder modifyReminder(@RequestBody Reminder reminder, @PathVariable long id) {
+        Reminder newReminder = remindService.modifyReminder(reminder, id);
+        return newReminder;
+    }
 
 }
