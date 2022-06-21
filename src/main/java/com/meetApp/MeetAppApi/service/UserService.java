9e08e8525f464724edfe939151d0a19359854fba
Mery05 <freiria_may@hotmail.com>
2022-06-21 18:40:51 +0200

    Métodos de User

--- src/main/java/com/meetApp/MeetAppApi/service/UserService.java
+++ src/main/java/com/meetApp/MeetAppApi/service/UserService.java
@@ -1,4 +1,16 @@
 package com.meetApp.MeetAppApi.service;
 
+import com.meetApp.MeetAppApi.domain.Message;
+import com.meetApp.MeetAppApi.domain.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> findAllUsers();
+
+    User deleteUser(long id);
+
+    User addUser(User user);
+
+    User modifyUser(User user, long id);
 }
