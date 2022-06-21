19a70ea262d71daea570a02a50d97a1e1ff98a63
Josete Meléndez <josetemelendez@gmail.com>
2022-06-21 16:43:21 +0200

    Métodos de los Events

--- src/main/java/com/meetApp/MeetAppApi/repository/EventRepository.java
+++ src/main/java/com/meetApp/MeetAppApi/repository/EventRepository.java
@@ -4,6 +4,10 @@ import com.meetApp.MeetAppApi.domain.Event;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public interface EventRepository  extends CrudRepository<Event, Long> {
+public interface EventRepository extends CrudRepository<Event, Long> {
+
+    List<Event> findAll();
 }
--- src/main/java/com/meetApp/MeetAppApi/service/EventService.java
+++ src/main/java/com/meetApp/MeetAppApi/service/EventService.java
@@ -1,4 +1,15 @@
 package com.meetApp.MeetAppApi.service;
 
+import com.meetApp.MeetAppApi.domain.Event;
+
+import java.util.List;
+
 public interface EventService {
+    List<Event> findAllEvents();
+
+    Event deleteEvent(long id);
+
+    Event addEvent(Event event);
+
+    Event modifyEvent(Event event, long id);
 }
