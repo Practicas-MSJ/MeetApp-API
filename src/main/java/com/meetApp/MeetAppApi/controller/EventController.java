45a3ba10ae4f4bf2ea6b52ba37fbe776929d6a14
Josete Meléndez <josetemelendez@gmail.com>
2022-06-22 17:44:08 +0200

    Logger creado y funcionando

--- src/main/java/com/meetApp/MeetAppApi/controller/EventController.java
+++ src/main/java/com/meetApp/MeetAppApi/controller/EventController.java
@@ -1,15 +1,12 @@
 package com.meetApp.MeetAppApi.controller;
 
 import com.meetApp.MeetAppApi.domain.Event;
-import com.meetApp.MeetAppApi.domain.Message;
-import com.meetApp.MeetAppApi.service.CategoryService;
 import com.meetApp.MeetAppApi.service.EventService;
 import com.meetApp.MeetAppApi.service.RemindService;
 import com.meetApp.MeetAppApi.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,38 +28,53 @@ public class EventController {
     // Lista todos los eventos
     @GetMapping("/events")
     public List<Event> getEvents() {
+        logger.info("Prepare list for all events");
         List<Event> events;
 
+        logger.info("Find all events");
         events = eventService.findAllEvents();
 
+        logger.info("End find all events");
         return events;
     }
 
     // Busca un evento por ID
     @GetMapping("/event/{id}")
     public Event getEvents(@PathVariable long id) {
+        logger.info("Find event by ID: " + id);
         Event event = eventService.findEvent(id);
+
+        logger.info("Enda find event by ID: " + id);
         return event;
     }
 
     // Borra un evento
     @DeleteMapping("/events/{id}")
     public Event deleteEvent(@PathVariable long id) {
+        logger.info("Delete event with ID: " + id);
         Event event = eventService.deleteEvent(id);
+
+        logger.info("End delete event with ID: " + id);
         return event;
     }
 
     // Registra un nuevo evento
     @PostMapping("/event")
     public Event addEvent(@RequestBody Event event) {
+        logger.info("Register a new event", event);
         Event newEvent = eventService.addEvent(event);
+
+        logger.info("End register a new event", event);
         return newEvent;
     }
 
     // Modifica un evento
     @PutMapping("/event/{id}")
     public Event modifyEvent(@RequestBody Event event, @PathVariable long id) {
+        logger.info("Modify event with ID: " + id, event);
         Event newEvent = eventService.modifyEvent(event, id);
+
+        logger.info("End modify event with ID: " + id, event, newEvent);
         return newEvent;
     }
 
--- src/main/java/com/meetApp/MeetAppApi/controller/MessageController.java
+++ src/main/java/com/meetApp/MeetAppApi/controller/MessageController.java
@@ -2,13 +2,11 @@ package com.meetApp.MeetAppApi.controller;
 
 import com.meetApp.MeetAppApi.domain.Message;
 import com.meetApp.MeetAppApi.service.CategoryService;
-import com.meetApp.MeetAppApi.service.EventService;
 import com.meetApp.MeetAppApi.service.MessageService;
 import com.meetApp.MeetAppApi.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,38 +28,53 @@ public class MessageController {
     // Lista todos los mensajes
     @GetMapping("/messages")
     public List<Message> getMessages() {
+        logger.info("Prepare list for all messages");
         List<Message> messages;
 
+        logger.info("Find all messages");
         messages = messageService.findAllMessages();
 
+        logger.info("End find all messages");
         return messages;
     }
 
     // Busca un mensaje por ID
     @GetMapping("/message/{id}")
     public Message getMessages(@PathVariable long id) {
+        logger.info("Get message by ID: " + id);
         Message message = messageService.findMessage(id);
+
+        logger.info("End get message by ID: " + id);
         return message;
     }
 
     // Borra un mensaje
     @DeleteMapping("/messages/{id}")
     public Message deleteMessage(@PathVariable long id) {
+        logger.info("Delete message with ID: " + id);
         Message message = messageService.deleteMessage(id);
+
+        logger.info("End delete message with ID: " + id);
         return message;
     }
 
     // Registra un nuevo mensaje
     @PostMapping("/message")
     public Message addMessage(@RequestBody Message message) {
+        logger.info("Register a new message", message);
         Message newMessage = messageService.addMessage(message);
+
+        logger.info("End register a new message", message);
         return newMessage;
     }
 
     // Modifica un mensaje
     @PutMapping("/message/{id}")
     public Message modifyMessage(@RequestBody Message message, @PathVariable long id) {
+        logger.info("Modify message with ID: " + id, message);
         Message newMessage = messageService.modifyMessage(message, id);
+
+        logger.info("End modify message with ID: " + id, message, newMessage);
         return newMessage;
     }
 }
